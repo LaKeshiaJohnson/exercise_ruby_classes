cdecc7558192678828f139f7f26f697d85e22e9b
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-10-09 11:29:25 -0500

    updated readme

---
+++ employees.rb
@@ -0,0 +1,14 @@
+class Company
+    
+    def initialize(company_name)
+        @company_name = company_name
+    end
+
+    #Returns the name of the company
+    def company_name
+        @company_name
+    end
+
+end
+
+# Add the remaining methods to fill the requirements above
